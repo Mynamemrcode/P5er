4acb25e16df7674aef522232ce4dac75b30166cb
Mynamemrcode <75869093+Mynamemrcode@users.noreply.github.com>
2021-07-12 13:16:30 -0700

    Add files via upload

---
+++ Thecoder.js
@@ -0,0 +1,24 @@
+function preload() {
+}
+function setup() {
+canvas=createCanvas(1000,500);
+canvas.position(125,320);
+video = createCapture(VIDEO);
+video.hide();
+tint_clr = "";
+}
+
+function draw() {
+image(video, 0, 0, 700, 500);
+tint(tint_clr);
+rect(0, 0, 5, 1000);
+rect(0, 0, 700, 5);
+rect(700, 0, 5, 1000);
+}
+function apply() {
+    tint_clr = document.getElementById("filterinput").value;
+    
+}
+function takeit() {
+    save("Tinted_Picture.png");
+}
